4816286d34442db6ded089d9c89fb4992cf49ff6
Maks <maksklimovbr@gmail.com>
2022-06-28 19:56:03 +0200

    + nav menu icons, togglers with styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,10 @@
 import Logo from 'components/Logo';
+import { ReactComponent as ClockSvg } from 'assets/icons/clock.svg';
+import { ReactComponent as StopwatchSvg } from 'assets/icons/stopwatch.svg';
+import { ReactComponent as PomodoroSvg } from 'assets/icons/pomodoro.svg';
+import { ReactComponent as TodoSvg } from 'assets/icons/todo.svg';
+import { ReactComponent as ThemeSvg } from 'assets/icons/theme.svg';
+import { ReactComponent as LangSvg } from 'assets/icons/lang.svg';
 import CustomNavLink from './CustomNavLink';
 
 import style from './Header.module.scss';
@@ -10,15 +16,31 @@ const Header = () => (
     </div>
     <nav className={style.nav}>
       <ul className={style.nav__list}>
-        <CustomNavLink to="/">Clock</CustomNavLink>
-        <CustomNavLink to="/stopwatch">Stopwatch</CustomNavLink>
-        <CustomNavLink to="/pomodoro">Pomodoro</CustomNavLink>
-        <CustomNavLink to="/todo">To Do</CustomNavLink>
+        <CustomNavLink to="/">
+          <ClockSvg className={style.nav__icon} />
+          Clock
+        </CustomNavLink>
+        <CustomNavLink to="/stopwatch">
+          <StopwatchSvg className={style.nav__icon} />
+          Stopwatch
+        </CustomNavLink>
+        <CustomNavLink to="/pomodoro">
+          <PomodoroSvg className={style.nav__icon} />
+          Pomodoro
+        </CustomNavLink>
+        <CustomNavLink to="/todo">
+          <TodoSvg className={style.nav__icon} />
+          To Do
+        </CustomNavLink>
       </ul>
     </nav>
-    <div className={style.headerBtns}>
-      <button type="button" className={style.themeBtn}>Theme</button>
-      <button type="button" className={style.langBtn}>Lang</button>
+    <div className={style.toggles}>
+      <button type="button" className={style.toggles__theme}>
+        <ThemeSvg className={style.toggles__themeSvg} />
+      </button>
+      <button type="button" className={style.toggles__lang}>
+        <LangSvg className={style.toggles__langSvg} />
+      </button>
     </div>
   </header>
 );
