88080ad0b733d596ffee0b0547842e0ac499c57f
Maks <maksklimovbr@gmail.com>
2022-06-29 14:39:54 +0200

    + footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
+import style from './Footer.module.scss';
+
 const Footer = () => (
-  <div>
-    Footer
+  <div className={style.Footer}>
+    2022 | By Maks Klimov | Open Source |
+    {' '}
+    <a href="https://github.com/MaksKlimov/zonal" target="_blank" rel="noreferrer">GitHub</a>
   </div>
 );
 
