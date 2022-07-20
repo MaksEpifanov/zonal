1e1320d5b7638e0f52c4c8c5c96e5962e33bc7ff
Maks <maksklimovbr@gmail.com>
2022-07-20 12:56:02 +0200

    + reset button in options modal (pomodoro)

--- src/pages/pomodoro/components/PomodoroOptions/OptionsInModal.jsx
+++ src/pages/pomodoro/components/PomodoroOptions/OptionsInModal.jsx
@@ -1,9 +1,11 @@
 import { ReactComponent as SoundSVG } from 'assets/icons/sound.svg';
 import { ReactComponent as MuteSVG } from 'assets/icons/mute.svg';
+import { ReactComponent as ResetSVG } from 'assets/icons/reset.svg';
 
 import Button from 'common/components/UI/Button';
 import NumberInput from 'common/components/UI/NumberInput';
 import useSettingsPomodoro from 'pages/pomodoro/hooks/useSettingsPomodoro';
+import useControlPomodoro from 'pages/pomodoro/hooks/useControlPomodoro';
 
 import styles from './PomodoroOptions.module.scss';
 
@@ -13,6 +15,8 @@ const OptionsInModal = () => {
     changeShortTime,
     changeLongTime,
     toggleSound] = useSettingsPomodoro();
+
+  const [,,,, resetTimer] = useControlPomodoro();
 
   const SoundButton = (
     <Button
@@ -24,7 +28,10 @@ const OptionsInModal = () => {
 
   return (
     <div className={styles.PomodoroOptions__modal}>
-      {SoundButton}
+      <div className={styles.PomodoroOptions__controls}>
+        {SoundButton}
+        <Button value="reset" icon={<ResetSVG />} onClick={resetTimer} />
+      </div>
       <NumberInput max={60} min={5} value={+settings.timerFocus} type="Focus time" onChange={changeFocusTime} />
       <NumberInput max={20} min={1} value={+settings.timerShortBreak} type="Short break time" onChange={changeShortTime} />
       <NumberInput max={45} min={5} value={+settings.timerLongBreak} type="Long break time" onChange={changeLongTime} />
