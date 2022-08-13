8d98cf6aefb6f0251c75293a1d6b1af65acf777d
Maks Epifanov <maksepifanovbr@gmail.com>
2022-08-13 23:05:13 +0300

    Edit footer

--- src/common/components/Footer/index.jsx
+++ src/common/components/Footer/index.jsx
@@ -2,9 +2,9 @@ import styles from './Footer.module.scss';
 
 const Footer = () => (
   <div className={styles.Footer}>
-    2022 | By Maks Klimov | Open Source |
+    2022 | By Maks Epifanov | Open Source |
     {' '}
-    <a href="https://github.com/MaksKlimov/zonal" target="_blank" rel="noreferrer">GitHub</a>
+    <a href="https://github.com/MaksEpifanov/zonal" target="_blank" rel="noreferrer">GitHub</a>
   </div>
 );
 
